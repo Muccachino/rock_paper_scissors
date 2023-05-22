449cba9919b2775f05c87c6a5d0e320e1cb8be27
JPcookie290 <julia.paier@yahoo.com>
2023-05-22 11:01:44 +0200

    first draft

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function playRound() {
     let pickComputer = computerPlay();
     let auswahl = prompt("Rock, Paper, Scissors? ")
     let hand = auswahl.toLowerCase();
-    console.log(pickComputer, typeof(pickComputer), hand, typeof(hand));
+    //console.log(pickComputer, typeof(pickComputer), hand, typeof(hand));
     if (pickComputer === hand) {
         //console.log("It is a Draw!");
         ergebnis = 0;
@@ -49,4 +49,28 @@ function playRound() {
         ergebnis = 2;
         return ergebnis
     } 
+}
+
+function game(zahl) {
+    let self = 0;
+    let computer = 0;
+    let ergebnis;
+    while (self < zahl && computer < zahl) {
+        ergebnis = playRound();
+        console.log("ergebnis: ", ergebnis);
+        if (ergebnis === 1){
+            self++;
+            console.log("You win this time!");
+        } else if (ergebnis === 2) {
+            computer++;
+            console.log("This time the computer wins!");
+        } else if (ergebnis === 0){
+            console.log("It's a Draw!\nPlay again.");
+        }
+    }
+    if (self === zahl) {
+        console.log("You won with " + String(self) + " points. The game is over!");
+    } else if (computer === zahl) {
+        console.log("You lost! The computer won with " + String(computer) + " points. The game is over!");
+    }  
 }
