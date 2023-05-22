44433c4489e20ed4e343a3d9ba44f6c41e42ffa3
JPcookie290 <julia.paier@yahoo.com>
2023-05-22 11:20:06 +0200

    second draft

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const howMany = function () {
+    let choice = parseInt(prompt("How often should be won?"))
+    return choice
+} 
+
 function computerPlay() {
     let pick;
     let zahl = Math.floor(Math.random() * 3); 
@@ -10,7 +15,6 @@ function computerPlay() {
     } else if (zahl === 2) {
         pick = "scissors";
     }
-    //console.log(zahl, pick);
     return pick;
 }
 
@@ -19,45 +23,37 @@ function playRound() {
     let pickComputer = computerPlay();
     let auswahl = prompt("Rock, Paper, Scissors? ")
     let hand = auswahl.toLowerCase();
-    //console.log(pickComputer, typeof(pickComputer), hand, typeof(hand));
     if (pickComputer === hand) {
-        //console.log("It is a Draw!");
         ergebnis = 0;
         return ergebnis
     } else if (pickComputer === "rock" && hand === "paper") {
-        //console.log("You win!\nPaper beats Rock.");
         ergebnis = 1;
         return ergebnis
     } else if (pickComputer === "rock" && hand === "scissors") {
-        //console.log("You lose!\nRock beats Scissors.");
         ergebnis = 2;
         return ergebnis
     } else if (pickComputer === "paper" && hand === "scissors") {
-        //console.log("You win!\nScissors beats Paper.");
         ergebnis = 1;
         return ergebnis
     } else if (pickComputer === "paper" && hand === "rock") {
-        //console.log("You lose!\nPaper beats Rock.");
         ergebnis = 2;
         return ergebnis
     } else if (pickComputer === "scissors" && hand === "rock") {
-        //console.log("You win!\nRock beats Scissors.");
         ergebnis = 1;
         return ergebnis
     } else if (pickComputer === "scissors" && hand === "paper") {
-        //console.log("You lose!\nScissors beats Paper.");
         ergebnis = 2;
         return ergebnis
     } 
 }
 
-function game(zahl) {
+function game() {
     let self = 0;
     let computer = 0;
     let ergebnis;
-    while (self < zahl && computer < zahl) {
+    const choice = howMany();
+    while (self < choice && computer < choice) {
         ergebnis = playRound();
-        console.log("ergebnis: ", ergebnis);
         if (ergebnis === 1){
             self++;
             console.log("You win this time!");
@@ -66,11 +62,13 @@ function game(zahl) {
             console.log("This time the computer wins!");
         } else if (ergebnis === 0){
             console.log("It's a Draw!\nPlay again.");
+        } else  {
+        console.log("Wrong input. Please choose between rock, paper or scissors!");
         }
     }
-    if (self === zahl) {
+    if (self === choice) {
         console.log("You won with " + String(self) + " points. The game is over!");
-    } else if (computer === zahl) {
+    } else if (computer === choice) {
         console.log("You lost! The computer won with " + String(computer) + " points. The game is over!");
     }  
 }
